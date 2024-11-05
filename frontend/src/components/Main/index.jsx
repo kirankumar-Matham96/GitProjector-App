4d23b4c7e0a41684cff90a214758bf300171160d
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2024-11-06 04:32:28 +0530

    feat: added landing page

--- frontend/src/components/Main/index.jsx
+++ frontend/src/components/Main/index.jsx
@@ -1,7 +1,21 @@
+import SplineRoboHead from "../SplineRoboHead";
 import styles from "./index.module.scss";
 
 const Main = () => {
-  return <div>Main Body content for Home page</div>;
+  return (
+    <main>
+      <section className={styles.hero}>
+        <SplineRoboHead />
+        <div className={styles.heroOverlay}>
+          <h1 className={styles.h1}>Git Projector</h1>
+          <h3 className={styles.subH}>
+          Uncover stats, trends, and insights from your code.
+          </h3>
+        </div>
+      </section>
+      <section className={styles.hero}></section>
+    </main>
+  );
 };
 
 export default Main;
